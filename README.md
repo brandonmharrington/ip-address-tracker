d70c6b56755bc7b4821cb83fa267086304f4938e
brandonmharrington <brandonmharrington@gmail.com>
2021-04-14 17:12:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./images/ip-address-tracker.png)
+
+### Links
+
+- Solution URL: [https://github.com/brandonmharrington/ip-address-tracker](https://github.com/brandonmharrington/ip-address-tracker)
+- Live Site URL: [https://brandonmharrington.github.io/ip-address-tracker](https://brandonmharrington.github.io/ip-address-tracker)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [IP Geolocation API](https://geo.ipify.org/)
+- [Leaflet JS](https://leafletjs.com/)
+
+### What I learned
+
+In this project, I got practice working with an API and a JS library that I had never used before. I also learned how to create a domain and IP address regex validator.
+
+### Useful resources
+
+- [IP Address validation](https://www.w3resource.com/javascript/form/ip-address-validation.php) - This helped me with validating IP Addresses. I had seen someone just checking for numbers 0-9 1-3 length, but this is the best IP Address regex check I could find for IPv4.
+
+## Author
+
+- Website - [Brandon Harrington](https://brandon-harrington.netlify.app)
+- Frontend Mentor - [@brandonmharrington](https://www.frontendmentor.io/profile/brandonmharrington)
+- Github - [@brandonmharrington](https://www.github.com/brandonmharrington)
---
+++ script.js
@@ -0,0 +1,86 @@
+const API_KEY = 'at_RTZGTfJNowjO2VAoYuYsLcY8M0bq7';
+const input = document.querySelector('.header__form-input');
+const ipData = document.querySelector('.result__ip p');
+const locationData = document.querySelector('.result__location p');
+const timezoneData = document.querySelector('.result__timezone p');
+const ispData = document.querySelector('.result__isp p');
+const btnSubmit = document.querySelector('.header__form-submit');
+const map = L.map('map');
+
+// display client IP address on page load
+const displayClientIP = async () => {
+  const res = await fetch('https://api.ipify.org?format=json');
+  const data = await res.json();
+  const clientIP = data.ip;
+
+  input.value = clientIP;
+  checkInput(clientIP);
+};
+displayClientIP();
+
+// listen for button click and fetch location data
+btnSubmit.addEventListener('click', e => {
+  e.preventDefault();
+
+  input.value
+    ? checkInput(input.value)
+    : alert('Please enter a valid IP Address or Domain.');
+});
+
+// check if user input is an IP address or a domain
+const checkInput = value => {
+  const domainFormat = /^(?!\-)([a-zA-Z0-9 \-?]{2,63})\.([a-zA-Z]{2,10})(.[a-z]{2,4})?$/;
+  const ipFormat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+  if (value.match(domainFormat)) {
+    fetchData(value, 'domain');
+  } else if (value.match(ipFormat)) {
+    fetchData(value, 'ipAddress');
+  } else {
+    alert('Invalid input. Please enter a valid IP Address or Domain.');
+  }
+};
+
+// fetch location data based on user input and generate info and map
+const fetchData = async (value, type) => {
+  try {
+    const url = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&${type}=${value}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    generateResults(data);
+    generateMap(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// generate location info
+const generateResults = data => {
+  const ip = data.ip;
+  const loc = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+  const timezone = `UTC ${data.location.timezone}`;
+  const isp = data.isp;
+
+  ipData.textContent = ip;
+  locationData.textContent = loc;
+  timezoneData.textContent = timezone;
+  ispData.textContent = isp;
+};
+
+// generate map
+const generateMap = data => {
+  const markerIcon = L.icon({
+    iconUrl: 'images/icon-location.svg',
+  });
+
+  map.setView([data.location.lat, data.location.lng], 15);
+
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  L.marker([data.location.lat, data.location.lng], { icon: markerIcon }).addTo(
+    map
+  );
+};
